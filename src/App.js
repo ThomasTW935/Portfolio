ea40efacbc188dcaf70d3678090ea9d76670c293
ThomasTW935 <thomastw935@gmail.com>
2021-04-26 12:10:42 +0800

    Added Navigation/About/Projects/ContactME with designs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.css'
+import testImg from './images/test.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>Portfolio</h2>
+        <nav>
+          <ul>
+            <li><a href='/'>About</a></li>
+            <li><a href='/'>Projects</a></li>
+            <li><a href='/'>Contact Me</a></li>
+          </ul>
+        </nav>
       </header>
+      <div className='aboutMe'>
+        <h1>Hi!</h1>
+      </div>
+      <div className='projects'>
+       
+        <div className='project'>
+          <div className='project__image'><img src={testImg} alt='test'/></div>
+          <h2 className='project__name'>Test</h2>
+          <p>File edit selection View go run Terminal Help App.js-React-Visual Studio Code</p>
+          <section className='project__buttons'>
+            <a href='/' className='github'>Github</a>
+            <a href='/' className='live'>Live Demo</a>
+          </section>
+        </div>
+        <div className='project'>
+          <div className='project__image'><img src={testImg} alt='test'/></div>
+          <h2 className='project__name'>Test</h2>
+          <p>File edit selection View go run Terminal Help App.js-React-Visual Studio Code</p>
+          <section className='project__buttons'>
+            <a href='/' className='github'>Github</a>
+            <a href='/' className='live'>Live Demo</a>
+          </section>
+        </div>
+        <div className='project'>
+          <div className='project__image'><img src={testImg} alt='test'/></div>
+          <h2 className='project__name'>Test</h2>
+          <p>File edit selection View go run Terminal Help App.js-React-Visual Studio Code</p>
+          <section className='project__buttons'>
+            <a href='/' className='github'>Github</a>
+            <a href='/' className='live'>Live Demo</a>
+          </section>
+        </div>
+      </div>
+      <div className='contactMe'>
+        <form>
+          <input type='text' placeholder='message'></input>
+        </form>
+      </div>
     </div>
   );
 }
