5f086d08b72d890573acf861313810b23cf99290
ThomasTW935 <thomastw935@gmail.com>
2021-05-01 13:47:02 +0800

    Fix style of project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/main.css'
 import testImg from './images/test.png'
+import WindowsCalculator from './images/Windows Calculator.PNG'
 
 function App() {
   return (
@@ -20,12 +21,12 @@ function App() {
       <div className='projects'>
        
         <div className='project'>
-          <div className='project__image'><img src={testImg} alt='test'/></div>
-          <h2 className='project__name'>Test</h2>
-          <p>File edit selection View go run Terminal Help App.js-React-Visual Studio Code</p>
+          <div className='project__image'><img src={WindowsCalculator} alt='test'/></div>
+          <h2 className='project__name'>Windows Calculator Clone</h2>
+          <p>Windows Calculator Clone using React with SASS</p>
           <section className='project__buttons'>
-            <a href='/' className='github'>Github</a>
-            <a href='/' className='live'>Live Demo</a>
+            <a target='_blank' rel='noreferrer' href='https://github.com/ThomasTW935/Windows-Calculator' className='github'>Github</a>
+            <a target='_blank' rel='noreferrer' href='https://thomastw935-windows-calculator.netlify.app/' className='live'>Live Demo</a>
           </section>
         </div>
         <div className='project'>
