5da1531c5c5f5a7240501f20bd17379ef0ee80bd
ThomasTW935 <thomastw935@gmail.com>
2021-05-02 10:42:55 +0800

    Added Skills Module

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 export default function Project({project}) {
-    const {image, title, message} = project
+    const {image, title, message, skills } = project
     const {github,live} = project.links
     return (
         <div className='project'>
           <div className='project__image'><img src={image} alt='test'/></div>
           <h2 className='project__name'>{title}</h2>
           <p>{message}</p>
+          <section className='project__icons'>
+            {
+              skills.map((skill,index)=>
+                <div className='project__icon'>
+                  <FontAwesomeIcon icon={skill.icon} size='2x' color={skill.color}/>
+                  <span>{skill.name}</span>
+                </div>
+              )
+            }
+          </section>
           <section className='project__buttons'>
             <a target='_blank' rel='noreferrer' href={github} className='github'>Github</a>
             <a target='_blank' rel='noreferrer' href={live} className='live'>Live Demo</a>
---
+++ src/data/Data.js
@@ -0,0 +1,64 @@
+import WindowsCalculator from '../images/Windows Calculator.PNG'
+import {faReact, faSass,faPhp, faJs, faHtml5} from '@fortawesome/free-brands-svg-icons'
+import {faFire, faDatabase, faServer} from '@fortawesome/free-solid-svg-icons'
+
+
+const ICONS = {
+  HTML:{name:'Html5', color:'#F16524', icon: faHtml5},
+  PHP:{name:'PHP', color:'#8993C1', icon: faPhp},
+  JAVASCRIPT:{name:'Javascript', color:'#F7E018', icon: faJs},
+  REACT:{name:'React', color:'#61DAFB', icon: faReact},
+  SASS:{name:'Sass', color: '#CF649A', icon: faSass},
+  FIREBASE:{name:'Firebase', color:'#FFCB2B', icon: faFire},
+  FIRESTORE:{name:'FireStore', color:'', icon: faDatabase},
+  MYSQL:{name:'MySql', color:'', icon: faDatabase},
+  HEROKU:{name:'Heroku', color:'', icon: faServer},
+}
+
+export const projects = [
+  {
+      image: WindowsCalculator, 
+      title: 'Windows Calculator Clone', 
+      message:'Windows Calculator Clone using React with SASS', 
+      skills: [
+          ICONS.REACT,
+          ICONS.SASS,
+      ],
+      links: {
+          github:'https://github.com/ThomasTW935/Windows-Calculator',
+          live: 'https://thomastw935-windows-calculator.netlify.app/'
+      }
+  },
+  {
+      image: WindowsCalculator, 
+      title: 'ToDo List', 
+      message:'Todo List with authentication using firebase', 
+      skills: [
+          ICONS.REACT,
+          ICONS.SASS,
+          ICONS.FIREBASE,
+          ICONS.FIRESTORE
+      ],
+      links: {
+          github:'https://github.com/ThomasTW935/Windows-Calculator',
+          live: 'https://thomastw935-windows-calculator.netlify.app/'
+      }
+  },
+  {
+      image: WindowsCalculator, 
+      title: 'Class Scheduling System', 
+      message:'', 
+      skills: [
+          ICONS.HTML,
+          ICONS.JAVASCRIPT,
+          ICONS.PHP,
+          ICONS.SASS,
+          ICONS.MYSQL,
+          ICONS.HEROKU,
+      ],
+      links: {
+          github:'https://github.com/ThomasTW935/Windows-Calculator',
+          live: 'https://thomastw935-windows-calculator.netlify.app/'
+      }
+  },
+]
