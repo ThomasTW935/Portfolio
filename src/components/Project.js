4bc84dd7de60eb565521b866295cbbd0f8e15758
ThomasTW935 <thomastw935@gmail.com>
2021-05-02 16:53:24 +0800

    Remove old code in Project Component

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,36 +6,17 @@ export default function Project({project}) {
     const {image, title, message, skills } = project
     const {github,live} = project.links
     return (
-        // <div className='project'>
-        //   <div className='project__image'><img src={image} alt='test'/></div>
-        //   <h2 className='project__name'>{title}</h2>
-        //   <p>{message}</p>
-        //   <section className='project__icons'>
-        //     {
-        //       skills.map((skill,index)=>
-        //         <div className='project__icon'>
-        //           <FontAwesomeIcon icon={skill.icon} size='2x' color={skill.color}/>
-        //           <span>{skill.name}</span>
-        //         </div>
-        //       )
-        //     }
-        //   </section>
-        //   <section className='project__buttons'>
-        //     <a target='_blank' rel='noreferrer' href={github} className='github'>Github</a>
-        //     <a target='_blank' rel='noreferrer' href={live} className='live'>Live Demo</a>
-        //   </section>
-        // </div>
         <div className='project'>
           <div className='project__image'><img alt={`${title}`} src={image}/></div>
           <div className='project__details'>
             <div className='project__title'>
               <h1>{title}</h1>
               <section className='project__cta'>
-                <a className='github project__skill' href='/'>
+                <a href={github} target='_blank' rel='noreferrer' className='github project__skill'>
                   <FontAwesomeIcon size='2x' icon={ICONS.GITHUB.icon} />
                   <span>Github</span>
                 </a>
-                <a className='live' href='/'>Live</a>
+                <a href={live} target='_blank' rel='noreferrer' className='live'>Live</a>
               </section>
             </div>
             <div className='project__skills'>
