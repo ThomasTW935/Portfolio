e9e44857d09cbcbd0f68e6984f69aa6d909194fa
ThomasTW935 <thomastw935@gmail.com>
2021-09-18 12:20:18 +0800

    Updated data

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,15 +1,15 @@
 import WindowsCalculator from '../images/Windows Calculator.PNG'
 import ToDoList from '../images/Todo List.jpg'
 import ClassSchedulingSystem from '../images/ClassSchedulingSystem.PNG'
-import {faReact, faSass,faPhp, faJs, faHtml5, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import {faReact, faSass,faPhp, faJs, faHtml5, faGithub, faLinkedin, faCss3Alt} from '@fortawesome/free-brands-svg-icons'
 import {faFire, faDatabase, faServer} from '@fortawesome/free-solid-svg-icons'
-
 
 export const ICONS = {
   HTML:{name:'Html5', color:'#F16524', icon: faHtml5},
   PHP:{name:'PHP', color:'#8993C1', icon: faPhp},
   JAVASCRIPT:{name:'Javascript', color:'#F7E018', icon: faJs},
   REACT:{name:'React', color:'#61DAFB', icon: faReact},
+  CSS:{name:'CSS', color: '#2965F1', icon: faCss3Alt},
   SASS:{name:'Sass', color: '#CF649A', icon: faSass},
   FIREBASE:{name:'Firebase', color:'#FFCB2B', icon: faFire},
   FIRESTORE:{name:'FireStore', color:'#FFCB2B', icon: faDatabase},
@@ -17,6 +17,7 @@ export const ICONS = {
   HEROKU:{name:'Heroku', color:'#430098', icon: faServer},
   GITHUB:{name:'Github', color:'', icon: faGithub},
   LINKEDIN:{name:'LinkedIn', color:'#0A66C2', icon: faLinkedin},
+  MONGODB:{name:'mongo DB', color:'#10AA50', icon: faDatabase},
 }
 
 export const projects = [
@@ -33,25 +34,25 @@ export const projects = [
           live: 'https://thomastw935-windows-calculator.netlify.app/'
       }
   },
-  {
-      image: ToDoList, 
-      title: 'ToDo List', 
-      description:'Todo List with authentication using firebase', 
-      skills: [
-          ICONS.REACT,
-          ICONS.SASS,
-          ICONS.FIREBASE,
-          ICONS.FIRESTORE
-      ],
-      links: {
-          github:'https://github.com/ThomasTW935/ToDoList-With-Authentication',
-          live:"https://todolist-with-authentication.netlify.app" 
-      }
-  },
+//   {
+//       image: ToDoList, 
+//       title: 'ToDo List', 
+//       description:'Todo List with authentication using firebase', 
+//       skills: [
+//           ICONS.REACT,
+//           ICONS.SASS,
+//           ICONS.FIREBASE,
+//           ICONS.FIRESTORE
+//       ],
+//       links: {
+//           github:'https://github.com/ThomasTW935/ToDoList-With-Authentication',
+//           live:"https://todolist-with-authentication.netlify.app" 
+//       }
+//   },
   {
       image: ClassSchedulingSystem, 
       title: 'Class Scheduling System', 
-      description:'Class Sche', 
+      description:'This system was created for our thesis. It is a system for the dean to assist in scheduling classes for the whole campus.', 
       skills: [
           ICONS.HTML,
           ICONS.JAVASCRIPT,
@@ -63,6 +64,22 @@ export const projects = [
       links: {
           github:'https://github.com/ThomasTW935/Class-Scheduling-System',
           live: 'https://aqueous-waters-36043.herokuapp.com/'
+      }
+  },
+  {
+      image: ClassSchedulingSystem, 
+      title: 'Facebook Clone', 
+      description:'A facebook clone ', 
+      skills: [
+          ICONS.REACT,
+          ICONS.CSS,
+          ICONS.FIREBASE,
+          ICONS.MONGODB,
+          ICONS.HEROKU,
+      ],
+      links: {
+          github:'https://github.com/ThomasTW935/fb-clone',
+          live: 'https://fb-clone-thomas.netlify.app/'
       }
   },
 ]
@@ -79,5 +96,11 @@ export const certificates = [
         company: "freeCodeCamp.org",
         date: "10/2020",
         link: "https://www.freecodecamp.org/certification/fcc87390ae7-8350-41ae-b8df-a9c18ace8fa9/javascript-algorithms-and-data-structures"
+    },
+    {
+        title: "Back End Development and APIs",
+        company: "freeCodeCamp.org",
+        date: "5/2021",
+        link: "https://www.freecodecamp.org/certification/fcc87390ae7-8350-41ae-b8df-a9c18ace8fa9/back-end-development-and-apis"
     },
 ]
