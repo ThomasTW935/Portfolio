357071a5e1cae523fafbf543e05008f947557bd8
Daryl Thomas <thomastw935@gmail.com>
2021-10-02 06:46:12 +0800

    Fix About imports

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { faEnvelopeSquare, faFilePdf } from "@fortawesome/free-solid-svg-icons"
 import { ICONS } from "../../data/Data"
 
 export default function About() {
@@ -9,12 +6,10 @@ export default function About() {
       <h1>Hi!</h1>
       <div className="contact">
         <p>
-          <FontAwesomeIcon icon={faEnvelopeSquare} />
+          {/* <FontAwesomeIcon icon={faEnvelopeSquare} /> */}
           <span>thomastw935@gmail.com</span>
         </p>
-        <p>
-          <FontAwesomeIcon icon={faFilePdf} />
-        </p>
+        <p>{/* <FontAwesomeIcon icon={faFilePdf} /> */}</p>
         <div className="links">
           <a
             href="https://github.com/ThomasTW935"
