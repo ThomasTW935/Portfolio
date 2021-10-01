fb7bfd185edb21e13f8c7fd82bc4d39a3cf3c41e
Daryl Thomas <thomastw935@gmail.com>
2021-10-02 06:16:38 +0800

    created a index file in components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Projects from "./Projects"
+import Nav from "./Nav"
+import Contacts from "./Contacts"
+import Certificates from "./Certificates"
+
+export { Projects, Nav, Contacts, Certificates }
