b97c445083e4dfcaf798b6ad876c2bec815133cc
Daryl Thomas <dthomas@glimsol.com>
2022-06-05 21:23:35 +0800

    Add theme component

---
+++ src/core-ui/theme.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme } from 'styled-components'
+
+export const defaultTheme: DefaultTheme = {
+  device: {
+    sm: '425px',
+    md: '768px',
+    lg: '1024px',
+  },
+  palette: {
+    common: {
+      black: '#111',
+      white: '#eee',
+    },
+    primary: {
+      main: '#7510F7 ',
+      shadow: '#0c5eca',
+    },
+    secondary: {
+      main:'#141C3A',
+    }
+  },
+}
